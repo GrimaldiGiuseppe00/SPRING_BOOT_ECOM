590079eb13fab4ec45a4c211e9e9b905d36d7107
Giuseppe Grimaldi <grimaldi.giuseppe@outlook.com>
2025-12-22 22:38:23 +0100

    CREATO CONTROLLER+SERVER (START SPRING WEB) E VERIFICATO API CON POSTMAN

--- src/main/java/com/ecommerce/Ecom/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/Ecom/service/CategoryServiceImpl.java
@@ -1,13 +1,18 @@
 package com.ecommerce.Ecom.service;
 
 import com.ecommerce.Ecom.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CategoryServiceImpl implements CategoryService {
     private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
     @Override
     public List<Category> getAllCategories() {
         return categories;
@@ -15,6 +20,30 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        category.setId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c -> c.getId().equals(categoryId)).findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+                ;
+        categories.remove(category);
+        return "Category with id"+ categoryId +"deleted successfully";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> optionalCategory = categories.stream()
+                .filter(c -> c.getId().equals(categoryId)).findFirst();
+        ;
+        if (optionalCategory.isPresent()) {
+            Category existingCategory = optionalCategory.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
+        }
     }
 }
